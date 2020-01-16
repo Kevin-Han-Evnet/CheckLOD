14c1a294b58a497943cc0b8b73e4e5cd20e94466
한상철 <playonflash@Gamil.com>
2020-01-16 12:42:24 +0900

    First gogogo

---
+++ CheckLOD-M/build/generated/source/buildConfig/debug/com/netmania/checklod/org/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netmania.checklod.org;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netmania.checklod.org";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "Alpha 1.2.7";
+}
